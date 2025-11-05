40b58fbe032d9a0912386862f556fe0d322e38e6
anna02vieira <annav@id.uff.br>
2025-11-05 10:54:08 -0300

    Update AnaliseExploratoria.R

--- AnaliseExploratoria.R
+++ AnaliseExploratoria.R
@@ -1,6 +1,8 @@
+# carregando pacotes
 library(readxl)
 library(tidyverse)
 library(dplyr)
+# lendo a base de dados
 base = read_xlsx("Base_trabalho.xlsx")
 base$escolaridade = as.factor(base$escolaridade)
 base$idade = as.factor(base$idade)
